3c69e8627a135bcf58af957bd1480e9bf0e76942
Britodfbr <britodfbr@gmail.com>
2021-07-28 08:13:34 -0300

    Modelo para usuário

--- flask_auth_app/models.py
+++ flask_auth_app/models.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = '@britodfbr'
+__author__ = "@britodfbr"
+from . import db
+
+
+class User(db.Model):
+    id = db.Column(
+        db.Integer, primary_key=True
+    )  # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
