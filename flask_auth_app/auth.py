62f5587bda689e9c8634a2b47f7894478d81c039
Britodfbr <britodfbr@gmail.com>
2021-07-28 07:41:54 -0300

    blueprint auth

--- flask_auth_app/auth.py
+++ flask_auth_app/auth.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = '@britodfbr'
+__author__ = "@britodfbr"
+from flask import Blueprint
+from . import db
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
+def login():
+    return "Login"
+
+
+@auth.route("/signup")
+def signup():
+    return "Signup"
+
+
+@auth.route("/logout")
+def logout():
+    return "Logout"
