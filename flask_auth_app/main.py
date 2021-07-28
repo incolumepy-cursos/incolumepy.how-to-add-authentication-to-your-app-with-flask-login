6a2e2f1e769c959555efee9a0ecbe3fd8126cdf2
Britodfbr <britodfbr@gmail.com>
2021-07-28 07:42:05 -0300

    blueprint main

--- flask_auth_app/main.py
+++ flask_auth_app/main.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = '@britodfbr'
+__author__ = "@britodfbr"
+from flask import Blueprint
+from . import db
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+def index():
+    return "Index"
+
+
+@main.route("/profile")
+def profile():
+    return "Profile"
